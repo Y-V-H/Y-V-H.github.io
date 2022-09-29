805cd77da93c6cf577af8102758647ccc8426a37
Yahor Hurynovich <y.hurynovich@epam.com>
2022-09-29 20:11:40 +0400

    Task-7. Update select and select handler

--- my-app/src/components/molecules/sort-by/sort-by.tsx
+++ my-app/src/components/molecules/sort-by/sort-by.tsx
@@ -1,65 +1,33 @@
 import React from "react";
 import Select from 'react-select';
+import { FilmSelect } from '../film-select/film-select'
 import { updateFilmsData } from '../../../toolkit-store/slicer-reducer';
 import { useDispatch  } from 'react-redux';
 
-const option = [
-    {value: 'rating', label: 'Rating'},
-    {value: 'genre', label: 'Genre'},
-    {value: 'release date', label: 'Release date'}
-]
+const getMovies = async (sortByValue?:string) => {
+    const url = `http://localhost:4000/movies${sortByValue ? `?sortBy=${sortByValue}` : ''}`;
+    const response = await (await fetch(url)).json();
+
+    return response.data;
+}
 
 export const SortBy = () => {
     const dispatch = useDispatch();
 
-    const handleChange = (selectOption: {value: string, label: string}) => {
-        dispatch(sortFilmsByFilter(selectOption.value))
+    const sortFilmsByFilter = async (sortBy: string) => {
+        const movies = await getMovies(sortBy);
+
+        dispatch(updateFilmsData(movies));
     }
 
-    const sortFilmsByFilter = (sortBy: string) => {
-        return async (dispatch: any) => {
-            fetch(`http://localhost:4000/movies`)
-            .then(( response ) => response.json())
-            .then( res => {
-                dispatch(updateFilmsData(res.data))
-            })
-        }
-    }
-
-    const colorStyle = {
-        control: (styles: any) => ({
-            ...styles,
-            backgroundColor: 'inherit',
-            border: '0',
-            fontFamily: 'montserrat-regular',
-            fontSize: '16px',
-            textTransform: 'uppercase',
-            cursor: 'pointer',
-            color: 'red'
-        }),
-        dropdownIndicator: (styles: any) => ({
-            ...styles,
-            color: "#F65261"
-        }),
-        singleValue: (styles: any) => ({
-            ...styles,
-            color: '#fff',
-            option: '0.1'
-        })
+    const handleChange = async (selectOption: {value: string, label: string}) => {
+        await sortFilmsByFilter(selectOption.value);
     }
 
     return (
         <div className="m-sort-by">
             <span className="m-sort-by__text">SORT BY</span>
-            {/* <button className="m-sort-by__btn">release date</button> */}
-            <Select
-                options={option}
-                onChange={handleChange}
-                styles={colorStyle}
-                components={{
-                    IndicatorSeparator: () => null
-                }}
-            />
+            <FilmSelect handleChange={handleChange}/>
         </div>
     )
 } 
